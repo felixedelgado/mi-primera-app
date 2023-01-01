8c0dc88922438b2583ad30e3bf5caa3f80db49c8
felixedelgado <felixedelgado@gmail.com>
2023-01-01 16:54:49 -0300

    Fin del 3er cap

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -6,14 +6,25 @@ import { Component } from "@angular/core";
     // Tambien podemos directamente escribir el HTML si no es muy extenso en el component
     // El styleUrl es opcional, el template es obligatorio
     templateUrl: './personas.component.html',
-    // styleUrls: ['./personas.component.css']
-    styles: [`
-    h1 {
-        color: blue;
-    }
-    `]
+    styleUrls: ['./personas.component.css']
+    // styles: [`
+    // h1 {
+    //     color: blue;
+    // }
+    // `]
 })
 // Esta es la clase asociada a nuestro nuevo componente
 export class PersonasComponent {
+    deshabilitar = false;
+    mensaje = 'No se ha agregado ninguna persona';
+    titulo = 'Ingeniero';
+    agregarPersona() {
+        this.mensaje = 'Persona agregada';
+    }
+    modificarTitulo(event: Event) {
+        // Para acceder al valor de la variable event
+        // Ademas hay que indicarle que es de tipo HTMLInputElement
+        this.titulo = (<HTMLInputElement>event.target).value;
+    }
 
 }
